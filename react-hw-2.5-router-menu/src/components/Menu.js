f27b3ed06eac914ae999fee22c0e9cd422245c17
unknown <a.ekaterina323@gmail.com>
2021-10-20 15:06:05 +0300

    fix styles

--- react-hw-2.5-router-menu/src/components/Menu.js
+++ react-hw-2.5-router-menu/src/components/Menu.js
@@ -3,10 +3,10 @@ import { NavLink } from "react-router-dom";
 export default function Menu() {
     return (
         <nav class="menu">
-            <NavLink exact to='' class="menu__item" activeClassName='menu__item-active'>Главная</NavLink>
-            <NavLink exact to='/drift' class="menu__item" activeClassName='menu__item-active'>Дрифт-такси</NavLink>
-            <NavLink exact to='/timeattack' class="menu__item" activeClassName='menu__item-active'>Time Attack</NavLink>
-            <NavLink exact to='/forza' class="menu__item" activeClassName='menu__item-active'>Forza Karting</NavLink>
+            <NavLink exact to='' className="menu__item" activeClassName='menu__item-active'>Главная</NavLink>
+            <NavLink exact to='/drift' className="menu__item" activeClassName='menu__item-active'>Дрифт-такси</NavLink>
+            <NavLink exact to='/timeattack' className="menu__item" activeClassName='menu__item-active'>Time Attack</NavLink>
+            <NavLink exact to='/forza' className="menu__item" activeClassName='menu__item-active'>Forza Karting</NavLink>
         </nav>
     )
   }
